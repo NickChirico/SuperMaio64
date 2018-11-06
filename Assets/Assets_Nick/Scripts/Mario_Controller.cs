9c45ff788942c091612553320f1fa327a295a393
Nick <nickychirico@mac.com>
2018-11-06 15:35:38 -0500

    Initial Commit to SourceTree

---
+++ Assets/Assets_Nick/Scripts/Mario_Controller.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario_Controller : MonoBehaviour {
+
+    public float MoveSpeed;
+    public float JumpForce;
+
+    private Rigidbody rb;
+    private Vector3 inputVector;
+    private Vector3 jumpVector;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        jumpVector = new Vector3(0.0f, 10.0f, 0.0f);
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateJump();
+    }
+
+    private void UpdateMovement()
+    {
+        float moveVelocity_X = MoveSpeed * Input.GetAxis("Horizontal");
+        float moveVelocity_Z = MoveSpeed * Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(moveVelocity_X, rb.velocity.y, moveVelocity_Z);
+
+
+
+        /*float hoz = MoveSpeed * Input.GetAxis("Horizontal");
+        float vert = MoveSpeed * Input.GetAxis("Vertical");
+
+        inputVector = new Vector3(hoz, 0f, vert);
+
+        if (inputVector != Vector3.zero)
+        {
+            this.transform.rotation = Quaternion.LookRotation(inputVector);
+        }*/
+
+
+    }
+
+    private void UpdateJump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Jump!");
+            rb.velocity = Vector2.up * JumpForce;
+
+
+            //rb.AddForce(jumpVector * JumpForce, ForceMode.Impulse);
+            //isGrounded = false;
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.velocity = inputVector;
+        //rb.velocity += jumpVector;
+    }
+
+}
---
+++ Assets/Assets_Nick/Scripts/PlayerJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float fallMultiplier = 3.25f;
+    public float lowJumpMultiplier = 3.1f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            //Maximum jump/normal jump
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        //if you let go of jump early, you jump shorter
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
